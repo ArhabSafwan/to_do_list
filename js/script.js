036275993de378fd10ca78c2db6385aca54e75e5
S.M ARHAB SAFWAN <75558126+ArhabSafwan@users.noreply.github.com>
2024-01-28 17:13:58 +0600

    v3

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function addTask() {
+  var inputTask = document.getElementById("input_Task");
+  var taskList = document.getElementById("task_list");
+
+    if (inputTask.value==='') {
+    alert("Add Task!");
+    }
+    else{
+        
+    }
+}
