450a91177bc438eef6a7b9019656db3f8014ed91
S.M ARHAB SAFWAN <75558126+ArhabSafwan@users.noreply.github.com>
2024-01-31 17:27:04 +0600

    v6

--- js/script.js
+++ js/script.js
@@ -6,23 +6,36 @@ btn.addEventListener("click", addList);
 function addList() {
   const assigned = document.querySelector(".assigned");
   const Completed = document.querySelector(".completed");
+  
 
   const newLi = document.createElement("li");
   const checkBtn = document.createElement("button");
   const delBtn = document.createElement("button");
   const unCheckBtn = document.createElement("button");
+  const date = document.createElement('p');
+  
 
   checkBtn.innerHTML = '<i class="fa fa-check"></i>';
   delBtn.innerHTML = '<i class="fa fa-trash"></i>';
   unCheckBtn.innerHTML = '<i class="fa fa-angle-up"></i>';
-
+  var options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true, // Set to true for 12-hour format, false for 24-hour format
+  };
+  date.innerHTML = new Date().toLocaleDateString("en-us", options);
 
   if (input.value !== "") {
     newLi.textContent = input.value;
     input.value = "";
     assigned.appendChild(newLi);
     newLi.appendChild(checkBtn);
-    newLi.appendChild(delBtn);  
+    newLi.appendChild(delBtn);
+    newLi.appendChild(unCheckBtn);
+    newLi.appendChild(date);
   }
 
   checkBtn.addEventListener("click", function () {
@@ -32,6 +45,7 @@ function addList() {
     checkBtn.style.display = "none";
     newLi.appendChild(unCheckBtn);
     console.log(unCheckBtn);
+    newLi.appendChild(date);
   });
 
   unCheckBtn.addEventListener("click", function () {
@@ -40,10 +54,9 @@ function addList() {
     assigned.appendChild(newLi);
     newLi.appendChild(checkBtn);
     newLi.appendChild(delBtn);
-    checkBtn.style.display = "inline"; 
-    unCheckBtn.style.display = "none";
+    checkBtn.style.display = "inline";
+    newLi.appendChild(date);
   });
-
 
   delBtn.addEventListener("click", function () {
     const parent = this.parentNode;
